e11064ed01b2fe5a617f537bcac226ce3bb04a6c
TianheWu <tianhe_wu@foxmail.com>
2021-12-23 19:25:33 +0800

    iet: tools finished

---
+++ temp.py
@@ -0,0 +1,8 @@
+a = [1, 4, 7, 0, 20]
+
+a.sort(reverse=True)
+print(a)
+
+b = 20
+s = a.index(b)
+print(s)
--- utils/tools.py
+++ utils/tools.py
@@ -9,8 +9,8 @@ def _get_subject_id(subject_name):
     return Subject.get(Subject.name == subject_name)
 
 
-def get_subject_score(user_id, subject):
-    sid = _get_subject_id(subject)
+def get_subject_score(user_id, subject_name):
+    sid = _get_subject_id(subject_name)
     return Grade.get(Grade.uid == user_id, Grade.subject_id == sid).score
 
 
@@ -18,7 +18,8 @@ def _get_all_grade_w_weight(user_id):
     grades = Grade.select().where(Grade.uid == user_id)
     return [{
         "score": x.score,
-        "credit": x.subject_id.credit
+        "credit": x.subject_id.credit,
+        "type": x.subject_id.type
     } for x in grades]
 
 
@@ -26,22 +27,47 @@ def _calc_weighted_average(all_grade):
     all_score = 0
     all_credit = 0
     for g in all_grade:
-        all_score += g['score']
+        if g['type'] == SubjectType.校选修课:
+            continue
+        all_score += g['score'] * g['credit']
         all_credit += g['credit']
     return all_score / all_credit
 
 
-def get_rank_from_class(user_id):
+def _get_class_user_id(class_id):
+    query = User.select().where(User.student_id != "", User.class_id_id == class_id)
+    return [{
+        "uid": u.uid,
+        "name": u.name,
+    } for u in query]
 
-    pass
+
+def _get_all_weighted_average_score(class_user_id):
+    class_weighted_average_score_list = []
+    for user in class_user_id:
+        user_all_grade = _get_all_grade_w_weight(user.uid)
+        user_weighted_average_score = _calc_weighted_average(user_all_grade)
+        class_weighted_average_score_list.append(user_weighted_average_score)
+    return class_weighted_average_score_list
+
+
+def get_rank_from_class(user_id):
+    class_id = User.select().where(User.uid == user_id)
+    class_user_id = _get_class_user_id(class_id)
+    class_weighted_average_score_list = _get_all_weighted_average_score(class_user_id)
+    class_weighted_average_score_list.sort(reverse=True)
+    all_grade = _get_all_grade_w_weight(user_id)
+    user_weighted_average_score = _calc_weighted_average(all_grade)
+    return class_weighted_average_score_list.index(user_weighted_average_score)
 
 
 def get_pe_test_score(user_id):
-    pass
+    get_subject_score(user_id, "体测")
 
 
 def is_has_innovation(user_id):
-    pass
+    query = Award.select().where(Award.uid == user_id)
+    return query.count() > 0
 
 
 def is_has_academic(user_id):
